a1aaf0012215a337cebdd59d9a7549f8642cd11f
ysr <141933879+jysr0@users.noreply.github.com>
2025-08-22 17:13:56 +0100

    Add files via upload

--- dmenu_wallpapers.sh
+++ dmenu_wallpapers.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-[ -d $XDG_PICTURES_DIR/wallpapers ] || exit 1
-sel_wl=$(ls -A $XDG_PICTURES_DIR/wallpapers/ | dmenu -l 20)
-xwallpaper --stretch $XDG_PICTURES_DIR/wallpapers/$sel_wl
+
+[ -d $XDG_PICTURES_DIR/wallpapers ] && [ -f $HOME/.xinitrc ] || exit 1
+	sel_wl=$(ls -A $XDG_PICTURES_DIR/wallpapers/ | dmenu -l 20)
+	
+	xwallpaper --stretch $XDG_PICTURES_DIR/wallpapers/$sel_wl && \
+	sed -i "s/xwallpaper \-\-stretch.*/xwallpaper \-\-stretch \$XDG_PICTURES_DIR\/wallpapers\/$sel_wl/g" $HOME/.xinitrc
