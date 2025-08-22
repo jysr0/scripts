3495220f34a603f14e84e46ce15baa51ec667b9d
ysr <141933879+jysr0@users.noreply.github.com>
2025-08-22 17:50:53 +0100

    Add files via upload

--- dwmblocks_battery.sh
+++ dwmblocks_battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(acpi -b | awk --field-separator=, '{ print $2 }' | tr -d ' ,%')" -lt 33 ] && [ "$(acpi -b | awk '{ print $3 }' | tr -d ',')" == "Discharging" ]; then
-	notify-send -u critical -t 10000 -a dunst '🪫 BATTERY LOW'
+	notify-send -u critical -t 7000 -a dunst '🪫 BATTERY LOW'
 fi
 	
-echo "🔋 $(acpi -b | awk --field-separator=, '{ print $2 }' | tr -d ' ')"
+echo "BAT: $(acpi -b | awk --field-separator=, '{ print $2 }' | tr -d ' ')"
