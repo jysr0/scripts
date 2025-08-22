3d3989f13adf0407cbdcab3f4a4b4f9b9af9ee8d
ysr <141933879+jysr0@users.noreply.github.com>
2025-08-22 18:56:16 +0100

    Add files via upload

--- dmenu_emojis.sh
+++ dmenu_emojis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 de="dmenu_emojis"
-[ -f "$XDG_DOCUMENTS_DIR/$de" ] || exit 1
+[ -f "$XDG_DOCUMENTS_DIR/$de" ]
 	emoji="$(cat $XDG_DOCUMENTS_DIR/$de \
 	| dmenu -i -l 20 -p Emojis: \
 	| awk -F',' '{ print $1 }')"
-	
-	echo "$emoji" | tr -d '\n, ' | xclip -selection clipboard && \
-	notify-send "$emoji copied to clipboard"       	
+		
+	echo "$emoji" | tr -d '\n' | xclip -selection clipboard
