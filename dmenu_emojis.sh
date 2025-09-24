44759a9b576cedb42867c8e0ceb122b70e92e99b
ysr <141933879+jysr0@users.noreply.github.com>
2025-09-24 13:12:50 +0100

    Update dmenu_emojis.sh

--- dmenu_emojis.sh
+++ dmenu_emojis.sh
@@ -4,6 +4,6 @@
 
 file="dmenu_emojis.txt"
 [[ -f "$XDG_DOCUMENTS_DIR/$file" ]] || notify-send "$file file does not exist!"
-	emoji="$(cat $XDG_DOCUMENTS_DIR/$file | dmenu -i -l 10 -p '<esc> to exit | 👻 copy emoji:')"
+	emoji="$(cat $XDG_DOCUMENTS_DIR/$file | dmenu -i -l 10 -p 'copy emoji:')"
 	[[ -n "$emoji" ]] || exit 0
 	echo "$emoji" | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard && notify-send "'$emoji' copied!"
