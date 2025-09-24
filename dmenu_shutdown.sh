39949148a9e71e02269404e183a7dd9c1dc0f09f
ysr <141933879+jysr0@users.noreply.github.com>
2025-09-24 13:14:42 +0100

    Update dmenu_shutdown.sh

--- dmenu_shutdown.sh
+++ dmenu_shutdown.sh
@@ -2,14 +2,14 @@
 
 # requirements: dmenu systemd
 
-op1="🍃 shutdown"
-op2="💫 restart"
-op3="💤 sleep"
-op4="⚓ hibernate"
-ansy="⭕ yes"
-ansn="❌ no"
+op1="shutdown"
+op2="restart"
+op3="sleep"
+op4="hibernate"
+ansy="yes"
+ansn="no"
 
-operation="$((echo $op1 ; echo $op2 ; echo $op3 ; echo $op4) | dmenu -p '<esc> to exit | system shutdown... :')"
+operation="$((echo $op1 ; echo $op2 ; echo $op3 ; echo $op4) | dmenu -p 'system shutdown:')"
 
 if [[ $operation == "$op1" ]]; then
 	confirmation="$((echo "$ansn" ; echo  "$ansy") | dmenu -p "confirm '$operation':")"
