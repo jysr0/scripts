68fd1de148058d3c72785ced8a9e06cbccd4fdd7
ysr <141933879+jysr0@users.noreply.github.com>
2025-10-02 18:55:17 +0100

    Add files via upload

--- dmenu_shutdown.sh
+++ dmenu_shutdown.sh
@@ -5,22 +5,19 @@
 op1="shutdown"
 op2="restart"
 op3="sleep"
-#op4="hibernate"
 ansy="yes"
 ansn="no"
 
-operation="$((echo $op1 ; echo $op2 ; echo $op3 ; echo $op4) | dmenu -p 'system shutdown:')"
+operation=$((echo $op1; echo $op2; echo $op3) | dmenu -p 'system shutdown:')
 
-if [[ $operation == "$op1" ]]; then
-	confirmation="$((echo "$ansn" ; echo  "$ansy") | dmenu -p "confirm '$operation':")"
-	[[ $confirmation == "$ansy" ]] && systemctl poweroff
+if [[ $operation == $op1 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -p "confirm $operation:")
+	[[ $confirmation == $ansy ]] && systemctl poweroff
 	
-elif [[ $operation == "$op2" ]]; then
-	confirmation="$((echo "$ansn" ; echo "$ansy") | dmenu -p "confirm '$operation':")"
-	[[ $confirmation == "$ansy" ]] && systemctl reboot
+elif [[ $operation == $op2 ]]; then
+	confirmation=$((echo $ansn; echo $ansy) | dmenu -p "confirm $operation:")
+	[[ $confirmation == $ansy ]] && systemctl reboot
 
-elif [[ $operation == "op3" ]]; then
+elif [[ $operation == $op3 ]]; then
 	systemctl suspend
-#elif [[ $operation == "$op4" ]]; then
-#	systemctl hibernate
 fi
