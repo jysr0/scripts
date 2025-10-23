129e61154d25c38a64e645891c54c39a39daca54
ysr <141933879+jysr0@users.noreply.github.com>
2025-10-23 04:05:40 +0100

    Update dmenu_shutdown.sh

--- dmenu_shutdown.sh
+++ dmenu_shutdown.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# requirements: dmenu systemd
-
 op1="shutdown"
 op2="restart"
 op3="sleep"
@@ -9,15 +7,12 @@ ansy="yes"
 ansn="no"
 
 operation=$((echo $op1; echo $op2; echo $op3) | dmenu -p 'system shutdown:')
-
 if [[ $operation == $op1 ]]; then
 	confirmation=$((echo $ansn; echo $ansy) | dmenu -p "confirm $operation:")
 	[[ $confirmation == $ansy ]] && systemctl poweroff
-	
 elif [[ $operation == $op2 ]]; then
 	confirmation=$((echo $ansn; echo $ansy) | dmenu -p "confirm $operation:")
 	[[ $confirmation == $ansy ]] && systemctl reboot
-
 elif [[ $operation == $op3 ]]; then
 	systemctl suspend
 fi
