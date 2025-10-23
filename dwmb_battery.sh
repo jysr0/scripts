7ee487c7b924282f63abfa7fe59ab04be691ad77
ysr <141933879+jysr0@users.noreply.github.com>
2025-10-23 04:12:38 +0100

    Update dwmb_battery.sh

--- dwmb_battery.sh
+++ dwmb_battery.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# optional: libnotify
-
 if [[ "$(cat /sys/class/power_supply/BAT1/capacity)" -lt 40 ]] && [[ "$(cat /sys/class/power_supply/BAT1/status)" == "Discharging" ]]; then
-	echo "🪫$(cat /sys/class/power_supply/BAT1/capacity)%"
+	echo "🪫 $(cat /sys/class/power_supply/BAT1/capacity)%"
 elif [[ "$(cat /sys/class/power_supply/BAT1/capacity)" -lt 40 ]] && [[ "$(cat /sys/class/power_supply/BAT1/status)" == "Charging" ]]; then
-	echo "🔌🪫$(cat /sys/class/power_supply/BAT1/capacity)%"
+	echo "🔌🪫 $(cat /sys/class/power_supply/BAT1/capacity)%"
 elif [[ "$(cat /sys/class/power_supply/BAT1/status)" == "Charging" ]]; then
-	echo "🔌🔋$(cat /sys/class/power_supply/BAT1/capacity)%"
+	echo "🔌🔋 $(cat /sys/class/power_supply/BAT1/capacity)%"
 else	
-	echo "🔋$(cat /sys/class/power_supply/BAT1/capacity)%"
+	echo "🔋 $(cat /sys/class/power_supply/BAT1/capacity)%"
 fi
