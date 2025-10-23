20274a4a01276fd774bc1e95c474da656c5c9d1e
ysr <141933879+jysr0@users.noreply.github.com>
2025-10-23 04:15:16 +0100

    Update dwmb_cpu.sh

--- dwmb_cpu.sh
+++ dwmb_cpu.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-echo "🔳$(mpstat -u | awk '/all/ {print $4}' | awk -F'.' '{print $1}')% 🌡$(sensors | tr -d '+' | awk '/^Package/ {print $4}')"
+echo "🔳 $(mpstat -u | awk '/all/ {print $4}' | awk -F'.' '{print $1}')% 🌡 $(sensors | tr -d '+' | awk '/^Package/ {print $4}')"
